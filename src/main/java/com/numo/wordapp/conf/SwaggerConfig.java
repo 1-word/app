896e80494658d3243a2b124a9b75ffc1166e0314
hyun <apfhd0257@naver.com>
2024-11-12 17:19:22 +0900

    update: api doc 토큰 사용 가능하도록 수정

---
+++ src/main/java/com/numo/wordapp/conf/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.numo.wordapp.conf;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Word API 명세서",
+                description = """
+                <Notice>
+                <p>* Responses의 *200은 200으로 응답되지만 예상 가능한 에러 처리입니다.</p>
+                """,
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+}
