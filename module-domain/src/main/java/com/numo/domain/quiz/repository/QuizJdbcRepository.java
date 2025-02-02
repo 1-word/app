df0f6d9fbb30f4c0944989bc89ad6ba243017a8d
hyun <apfhd0257@naver.com>
2025-02-02 12:59:53 +0900

    fix: 7일 이후 퀴즈 삭제 쿼리 변경 - 7일 전 조회하는 쿼리로 되어있어 퀴즈가 계속 삭제되는 이슈 수정 - 완료된 퀴즈만 삭제되도록 변경 -> 퀴즈 이어하기 데이터는 삭제 안하도록 수정

--- module-domain/src/main/java/com/numo/domain/quiz/repository/QuizJdbcRepository.java
+++ module-domain/src/main/java/com/numo/domain/quiz/repository/QuizJdbcRepository.java
@@ -15,11 +15,13 @@ public class QuizJdbcRepository {
 
     /**
      * 7일이 지난 퀴즈 데이터를 삭제한다
+     * 완료된 퀴즈 데이터만 삭제 (이어하기 데이터는 삭제하지 않음)
      */
     public void deleteQuiz() {
         String sql = "DELETE q FROM quiz q " +
                 "INNER JOIN quiz_info qi ON q.quiz_info_id = qi.quiz_info_id " +
-                "WHERE qi.create_time > NOW() - INTERVAL 7 DAY; ";
+                "WHERE qi.complete = true " +
+                " and qi.update_time < NOW() - INTERVAL 7 DAY;";
         jdbcTemplate.update(sql);
     }
 }
